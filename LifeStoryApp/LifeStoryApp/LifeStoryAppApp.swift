03a0f502b54f1d20d785529d5831b676dae39c9d
Anderson Kloss Maia <kloss@Andersons-MacBook-Air.local>
2022-12-27 10:45:21 -0300

    First commid

---
+++ LifeStoryApp/LifeStoryApp/LifeStoryAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  LifeStoryAppApp.swift
+//  LifeStoryApp
+//
+//  Created by Anderson Kloss Maia on 27/12/22.
+//
+
+import SwiftUI
+
+@main
+struct LifeStoryAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
